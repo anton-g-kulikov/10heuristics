bb65c2d216b8af8deb94caab2c27e509b9a7a94e
Антон Куликов <kulikov@selectel.ru>
2015-08-02 17:22:17 +0300

    some refactoring

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('cover').click(function(){
+	$('.cover').click(function(){
 		$(this).nextAll('.description').removeClass('visible').addClass('hidden');
 		$(this).nextAll('.gotit').removeClass('visible').addClass('hidden');
 		$(this).addClass('hidden');
@@ -11,17 +11,17 @@ $(function () {
 	});
 
 	$('.gotit').click(function(){
-		$(this).prevAll('cover').removeClass('hidden').addClass('visible');
+		$(this).prevAll('.cover').removeClass('hidden').addClass('visible');
 	});
 
 	$('#study').click(function(){
-		$('cover').removeClass('hidden').addClass('visible');
+		$('.cover').removeClass('hidden').addClass('visible');
 		$('.description').removeClass('visible').addClass('hidden');
 		$('.gotit').removeClass('visible').addClass('hidden');
 	});
 
 	$('#read').click(function(){
-		$('cover').removeClass('visible').addClass('hidden');
+		$('.cover').removeClass('visible').addClass('hidden');
 		$('.description').removeClass('hidden').addClass('visible');
 		$('.gotit').removeClass('hidden').addClass('visible');
 	});
