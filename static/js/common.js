06cd66ac9fcf3e2fc47a9406d405c53542c931bc
Антон Куликов <kulikov@selectel.ru>
2015-08-02 15:42:51 +0300

    content, design and interactivity

---
+++ static/js/common.js
@@ -0,0 +1,30 @@
+$(function () {
+	$('cover').click(function(){
+		$(this).nextAll('.description').removeClass('visible').addClass('hidden');
+		$(this).nextAll('.gotit').removeClass('visible').addClass('hidden');
+		$(this).addClass('hidden');
+	});
+
+	$('.definition').click(function(){
+		$(this).next('.description').toggleClass('hidden');
+		$(this).nextAll('.gotit').toggleClass('hidden');
+	});
+
+	$('.gotit').click(function(){
+		$(this).prevAll('cover').removeClass('hidden').addClass('visible');
+	});
+
+	$('#study').click(function(){
+		$('cover').removeClass('hidden').addClass('visible');
+		$('.description').removeClass('visible').addClass('hidden');
+		$('.gotit').removeClass('visible').addClass('hidden');
+	});
+
+	$('#read').click(function(){
+		$('cover').removeClass('visible').addClass('hidden');
+		$('.description').removeClass('hidden').addClass('visible');
+		$('.gotit').removeClass('hidden').addClass('visible');
+	});
+
+});
+
